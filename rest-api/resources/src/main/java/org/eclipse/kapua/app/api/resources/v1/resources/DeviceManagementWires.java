285e65911a8c320b096e24ae7556020bd9e65506
angelo.andreussi <angelo.andreussi@gmail.com>
2026-07-29 16:26:49 +0200

    :recycle: refactored path for wires api to be more future proof

--- rest-api/resources/src/main/java/org/eclipse/kapua/app/api/resources/v1/resources/DeviceManagementWires.java
+++ rest-api/resources/src/main/java/org/eclipse/kapua/app/api/resources/v1/resources/DeviceManagementWires.java
@@ -35,7 +35,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("{scopeId}/devices/{deviceId}/graph")
+@Path("{scopeId}/devices/{deviceId}/wire/graph")
 public class DeviceManagementWires extends AbstractKapuaResource {
 
     @Inject
