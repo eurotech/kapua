bdeb0c1607c906535fad5f14bfec8305442522e0
Alberto Codutti <alberto.codutti@eurotech.com>
2026-08-05 15:59:24 +0200

    :bookmark: [Project] Set modules version to 1.6.15
    Signed-off-by: Alberto Codutti <alberto.codutti@eurotech.com>

--- deployment/minishift/minishift-importer.sh
+++ deployment/minishift/minishift-importer.sh
@@ -29,7 +29,7 @@
 
 ERROR=0
 DOCKER_ACCOUNT=${DOCKER_ACCOUNT:=eclipsekapua}
-IMAGE_VERSION=${IMAGE_VERSION:=1.6.15-SNAPSHOT}
+IMAGE_VERSION=${IMAGE_VERSION:=1.6.15}
 SERVICES=("console" "api" "sql" "broker" "events-broker")
 TMP_DIR="/tmp/kapua-containers-$(date +%s)"
 
